a90e44a8ebb9964950f6a5b67e442c2c8d6676b0
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-02-01 09:43:27 +0100

    router

--- js/front-office/index.js
+++ js/front-office/index.js
@@ -1,4 +1,5 @@
-import "Navigation.jsx";
+
+/*require("Navigation.jsx");
 
 ReactDOM.render(
     <Router>
@@ -12,8 +13,34 @@ ReactDOM.render(
           <Route path=":objectSlug" element={<Post />} />
         </Route>
       </Routes>
-      <Footer /> */}
+      <Footer /> *//*}
     </Router>,
-  
+
     document.getElementById("root")
-);
+);*/
+
+
+
+
+const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+
+const App = () => (
+  <ReactRouterDOM.HashRouter>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/register">Register</Link></li>
+    </ul>
+
+    <Route path="/" exact component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/register" component={Register} />
+  </ReactRouterDOM.HashRouter>
+)
+
+const Home = () => <h4>Home</h4>
+const Login = () => <h1>Login</h1>
+const Register = () => <h1>Register</h1>
+
+ReactDOM.render(<App />, document.querySelector('#root'));
