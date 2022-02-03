184d6f587ddac0da4fcbc31d68cbd31e9212ef57
Francesco Vittori <fran@html24.net>
2022-02-03 21:01:25 +0100

    Home Fix

--- js/back-office.js
+++ js/back-office.js
@@ -74,14 +74,14 @@ function showHome() {
                 <button class="btn btn-dark btn-lg" id="noLogIn">Guest access</button>\
               </div>\
               <div class="col-12 col-lg-5">\
-                <form id="formEmployees" action="page-templates/back-office/authentication.php">\
+                <form id="formEmployees" action="http://localhost:8000/API/login">\
                   <div class="text-center">\
                     <img src="/img/logos/KITrental-logos_black.png" alt="logo">\
                   </div>\
                   <h3 class="fw-normal mb-2 pb-3 mt-4 pt-4 text-center" style="letter-spacing: 1px;">Log in</h3>\
                   <div class="form-outline mb-3">\
-                    <label class="form-label" for="emplId">Email address</label>\
-                    <input type="email" name="emplId" id="emplUsername" class="form-control form-control-md" required>\
+                    <label class="form-label" for="emplUsername">Email address</label>\
+                    <input type="email" name="emplUsername" id="emplUsername" class="form-control form-control-md" required>\
                   </div>\
                   <div class="form-outline mb-4">\
                     <label class="form-label" for="emplPw">Password</label>\
@@ -95,7 +95,7 @@ function showHome() {
               </div>\
           </div>\
         </div>');
-        $("body").append('<button onclick="showClients(); $(\'nav\').show();" class="btn btn-info btn-lg btn-block btn-dark" type="button">Clients</button>')
+        $("body").append('<button onclick="showClients()" class="btn btn-info btn-lg btn-block btn-dark" type="button">Clients</button>')
     }
 
 
@@ -122,27 +122,28 @@ function showHome() {
         $("#formEmployees h3").addClass("text-danger");
       }else{
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: actionUrl,
+            data: form.serialize(),
             success: function(data){
-              authentication(data.employees,form.serializeArray());
-              if(data==false){
+              console.log(data);
+              /*if(data==false){
                 $("#formEmployees h3").text("Log in - Wrong data inserted !");
                 $("#formEmployees h3").addClass("text-danger");
               }else
-                alert("SI");
+                alert("SI");*/
                 //location.reload();// show response from the php script.
             },
             error: function (xhr, ajaxOptions, thrownError) {
-              alert(xhr.status);
-              alert(thrownError);
+              console.log(xhr);
+              console.log(thrownError);
             }
         });
       }
     });
   }
 }
-
+    
 function authentication(employees,formData){
   for(var i = 0; i < employees.length; i++)
     if(employees[i].username == formData[0].value && employees[i].password == bcrypt.hashSync(formData[0].value, 14)){
@@ -505,123 +506,4 @@ function showResult(str) {
       else
         $(this).hide();
     });
-}
-
-
-$(document).ready(function(){
-  if(window.location.href.indexOf("backoffice") > -1){
-    if(loggedIn){
-      $("#content").append('\
-        <div class="container-fluid h-100">\
-            <div class="row h-100">\
-                <div id="clients-section" class="col-12 col-lg-4 bg-primary sectionsBack">\
-                    <a href="page-templates\back-office\clients.php" class="w-100 h-100">\
-                        <h1 class="text-white text-uppercase">Clients</h1>\
-                    </a>\
-                </div>\
-                <div id="inventory-section" class="col-12 col-lg-4 bg-secondary sectionsBack">\
-                    <a href="page-templates\back-office\inventory.php" class="w-100 h-100">\
-                        <h1 class="text-white text-uppercase">Inventory</h1>\
-                    </a>\
-                </div>\
-                <div id="rental-section" class="col-12 col-lg-4 bg-success sectionsBack">\
-                    <a href="page-templates\back-office\rental.php" class="w-100 h-100">\
-                        <h1 class="text-white text-uppercase">Rental</h1>\
-                    </a>\
-                </div>\
-            </div>\
-        </div>');
-    }else{
-      $("body").append('\
-      <div class="container-fluid h-100">\
-          <div class="row h-100" id="backendLogin">\
-              <div class="col-12 col-lg-7">\
-                <div id="titleCF">\
-                  <h2 class="text-white display-3 text-center ">Welcome in the</h2>\
-                </div>\
-                <div id="titleCS">\
-                  <h2 class="text-white display-3 text-center ">Back Office</h2>\
-                </div>\
-                <button class="btn btn-dark btn-lg" id="noLogIn">Guest access</button>\
-              </div>\
-              <div class="col-12 col-lg-5">\
-                <form id="formEmployees" action="http://localhost:8000/API/login">\
-                  <div class="text-center">\
-                    <img src="/img/logos/KITrental-logos_black.png" alt="logo">\
-                  </div>\
-                  <h3 class="fw-normal mb-2 pb-3 mt-4 pt-4 text-center" style="letter-spacing: 1px;">Log in</h3>\
-                  <div class="form-outline mb-3">\
-                    <label class="form-label" for="emplUsername">Email address</label>\
-                    <input type="email" name="emplUsername" id="emplUsername" class="form-control form-control-md" required>\
-                  </div>\
-                  <div class="form-outline mb-4">\
-                    <label class="form-label" for="emplPw">Password</label>\
-                    <input type="password" name="emplPw" id="emplPassword" class="form-control form-control-md" required>\
-                  </div>\
-                  <div class="pt-1 mb-4">\
-                    <button class="btn btn-info btn-lg btn-block btn-dark" type="button">Login</button>\
-                  </div>\
-                  <p class="small mb-5 pb-lg-2 text-center"><a class="text-muted" href="#!">Forgot password?</a></p>\
-                </form>\
-              </div>\
-          </div>\
-        </div>');
-        $("body").append('<button onclick="showClients()" class="btn btn-info btn-lg btn-block btn-dark" type="button">Clients</button>')
-    }
-
-
-
-    $( ".sectionsBack" ).each(function(){
-      $(this).hover(function(){
-        $(this).css("background-size","300%");
-        $("a h1",this).css("fontSize","4.5rem");
-      },function(){
-        $(this).css("background-size","250%");
-        $("a h1",this).css("fontSize","4rem");
-      });
-    });
-
-    $("#formEmployees .btn").click(function(e) {
-      var form = $("#formEmployees");
-      var actionUrl = form.attr('action');
-
-      if(!isEmail($("input[type='email']").val())){
-        $("#formEmployees h3").text("Log in - Insert valid email !");
-        $("#formEmployees h3").addClass("text-danger");
-      }else if($("input[type='password']").val() == ""){
-        $("#formEmployees h3").text("Log in - Insert password !");
-        $("#formEmployees h3").addClass("text-danger");
-      }else{
-        $.ajax({
-            type: "POST",
-            url: actionUrl,
-            data: form.serialize(),
-            success: function(data){
-              console.log(data);
-              /*if(data==false){
-                $("#formEmployees h3").text("Log in - Wrong data inserted !");
-                $("#formEmployees h3").addClass("text-danger");
-              }else
-                alert("SI");*/
-                //location.reload();// show response from the php script.
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-              console.log(xhr);
-              console.log(thrownError);
-            }
-        });
-      }
-    });
-  }
-});
-
-function authentication(employees,formData){
-  for(var i = 0; i < employees.length; i++)
-    if(employees[i].username == formData[0].value && employees[i].password == bcrypt.hashSync(formData[0].value, 14)){
-      console.log(employees[i].password == bcrypt.hashSync(formData[0].value, 14));
-      //return true;
-    }else{
-      console.log(employees[i].password == bcrypt.hashSync(formData[0].value, 14));
-    }
-  //return false;
 }
