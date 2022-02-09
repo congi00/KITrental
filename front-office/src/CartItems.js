70db5f9d585a3317c46e67b2482fa41d0ad74e36
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-02-09 18:08:21 +0100

    cart items thumb

--- front-office/src/CartItems.js
+++ front-office/src/CartItems.js
@@ -6,13 +6,24 @@ import { Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 import {Card} from "react-bootstrap"
 import ImageThumb from "./img/Blender.jpg"
+import ImageThumb2 from "./img/torch.jpg"
 
 function CartItems(){
   return(
     <div className="cartSection">
-      <Card>
+      <h1>CART</h1>
+      <Card className="cartItem">
         <Card.Body>
-          <img thumbnail="true" src={ImageThumb} />
+          <div className="thumbCart">
+            <img thumbnail="true" src={ImageThumb} />
+          </div>
+        </Card.Body>
+      </Card>
+      <Card className="cartItem">
+        <Card.Body>
+          <div className="thumbCart">
+            <img thumbnail="true" src={ImageThumb2} />
+          </div>
         </Card.Body>
       </Card>
     </div>
