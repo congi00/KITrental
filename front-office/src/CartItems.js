8f647009b3c977f8565baf58592b1ce291fe7793
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-02-09 19:15:24 +0100

    cart beginning design

--- front-office/src/CartItems.js
+++ front-office/src/CartItems.js
@@ -6,25 +6,65 @@ import { Link } from "react-router-dom";
 import {Card} from "react-bootstrap"
 import ImageThumb from "./img/Blender.jpg"
 import ImageThumb2 from "./img/torch.jpg"
+import {Form, Button }from 'react-bootstrap'
 
 function CartItems(){
+
   return(
     <div className="cartSection">
       <h1>CART</h1>
-      <Card className="cartItem">
-        <Card.Body>
-          <div className="thumbCart">
-            <img thumbnail="true" src={ImageThumb} />
-          </div>
-        </Card.Body>
-      </Card>
-      <Card className="cartItem">
-        <Card.Body>
-          <div className="thumbCart">
-            <img thumbnail="true" src={ImageThumb2} />
-          </div>
-        </Card.Body>
-      </Card>
+      <Form className="formCart">
+          <Card className="cartItem">
+            <Card.Body>
+              <div className="thumbCart">
+                <img thumbnail="true" src={ImageThumb} />
+              </div>
+              <div className="itemTitle">
+                <h2>Blender</h2>
+                <h3>Professional</h3>
+              </div>
+              <div className="itemPrice">
+                <h2>322$</h2>
+                <Form.Control type="number" placeholder="1" />
+              </div>
+            </Card.Body>
+          </Card>
+          <Card className="cartItem">
+            <Card.Body>
+              <div className="thumbCart">
+                <img thumbnail="true" src={ImageThumb2} />
+              </div>
+              <div className="itemTitle">
+                <h2>Torch</h2>
+                <h3>Professional</h3>
+              </div>
+              <div className="itemPrice">
+                <h2>322$</h2>
+                <Form.Control type="number" placeholder="1" />
+              </div>
+            </Card.Body>
+          </Card>
+          <Card className="cartItem">
+            <Card.Body>
+              <div className="thumbCart">
+                <img thumbnail="true" src={ImageThumb2} />
+              </div>
+              <div className="itemTitle">
+                <h2>Torch</h2>
+                <h3>Professional</h3>
+              </div>
+              <div className="itemPrice">
+                <h2>322$</h2>
+                <Form.Control type="number" placeholder="1" />
+              </div>
+            </Card.Body>
+          </Card>
+          <h2 className="totalItems">Total:</h2>
+          <h2 className="totalItemsPrice">130$</h2>
+          <Button variant="danger" type="submit">
+            Submit
+          </Button>
+      </Form>
     </div>
   );
 }
