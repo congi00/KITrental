809ad042fde9142d30fb2aaf19b3c938a1520a59
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-02-10 16:39:41 +0100

    working dashboard connected

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,32 +1,150 @@
 <template>
-  <div id="app">
-<!--    <div id="nav">
-      <router-link to="/">Home</router-link> |
+  <div id="home">
+    <section class="homeSection">
+      <div class="container-fluid">
+        <h1 class="title">Welcome into the Manager <br>Dashboard</h1>
+        <h2 class="subtitle">
+          make sure you are in the right place!
+        </h2>
+        <form class="formSection" v-on:submit="login">
+          <img src="./img/KITrental-logos_black.png">
+          <div class="field">
+            <label class="label">Username</label>
+            <div class="control">
+              <input v-model="username" type="text" placeholder="Username">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input type="password" v-model="password" placeholder="Password">
+            </div>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
+   <!--<div id="nav">
+      <router-link to="/dashboard">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>-->
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  //import router from "../router"    
+  import axios from "axios"  
+  
+  export default {
+    name: 'home',
+    components: {},
+    data(){
+      return{
+        username: '',
+        password: '',
+      }
+    },
+    methods: {
+      login: (e) => {    
+       e.preventDefault()    
+       let email = "a@a.aa"   
+       let password = "password"    
+       let login = () => {    
+        let data = {    
+          email: email,    
+          password: password    
+        }    
+        axios.post("/api/login", data)    
+          .then((response) => {    
+            console.log(response)    
+            //router.push("/dashboard")    
+          })    
+          .catch((errors) => {    
+            console.log(errors)    
+          })    
+        }    
+        login()
+      }  
+    },
+  };
+</script>
+<style lang="scss" >
+  @import url('https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap');
 
-#nav {
-  padding: 30px;
+  .homeSection{
+    width:100vw;
+    height:100vh;
+    background-color:#031f1c;
+    overflow:hidden;
+  }
+  .homeSection h1 {
+    color:#fff;
+    font-size: 4vw;
+    font-family: 'Jost', sans-serif;
+  }
+  .homeSection h2 {
+    color:#fff;
+    font-size: 2vw;
+    font-family: 'Jost', sans-serif;
+    font-weight:bold;
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  .homeSection .title{
+    position:absolute;
+    top:30vh;
+    left:2vw;
+    text-align:left;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  .homeSection .subtitle{
+    position:absolute;
+    top:55vh;
+    left:2vw;
+    text-align:left;
+  }
+
+  .formSection{
+    height:50vh;
+    width:25vw;
+    background-color: rgba(255, 255, 255, .45);
+    backdrop-filter: blur(10px);
+    border-radius:10px;
+    position:absolute;
+    right:5vw;
+    top:25vh;
+    text-align:left;
+    padding-left:2vw;
+    img{
+      width:10vw;
+      position:relative;
+      bottom:6vh;
+      left:5vw;
+    }
+    .field{
+      position:relative;
+      bottom:5vh;
+      input[type="text"],input[type="password"]{
+        width:90%;
+        height:4.5vh;
+        border:none;
+        padding-left:5px;
+      }
+      label{
+        color:#222;
+      }
+    }
+    button{
+      width:50%;
+      height:12%;
+      position:relative;
+      left:5vw;
+      bottom:2vh;
+      background:#222;
+      border:0;
+      border-radius:5px;
+      color:#fff;
     }
   }
-}
 </style>
+
