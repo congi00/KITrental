4aec7673af0f8dd0b9a05b187e7661e3ce205de9
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-03-03 11:14:02 +0100

    ss

--- front-office/src/CartItems.js
+++ front-office/src/CartItems.js
@@ -176,7 +176,7 @@ function CartItems(){
                   console.log(pricePItem)
                 }
               }
-              pricesProducts.push(pricePItem);
+              pricesProducts.push(parseInt(pricePItem));
               datesP = datesP.concat([{startDate : item.startD, endDate : item.endD}]);
               productsID.push(item._id);
               console.log(pricePItem)
