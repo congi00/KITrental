bff922f3b1b24e9214695a40b3385dce2d505a53
Alessandro Congiusti <alessandro.congiusti@studio.unibo.it>
2022-03-06 17:58:51 +0100

    csa

--- back-office/page-templates/back-office/API/invoice.js
+++ back-office/page-templates/back-office/API/invoice.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const path = require('path');
 
 //function createInvoice(infosPdf, path) {
-  async function createInvoice(docDefinition, successCallback, errorCallback) {
+  function createInvoice(docDefinition, successCallback, errorCallback) {
   try { 
     //const fontDescriptors = { ... };fontDescriptors
     var fonts = {
@@ -178,7 +178,7 @@ router.post('/', async function (req, res) {
 })
 
 
-router.post('/pdf/', async (req, res) => {
+router.post('/pdf/',  (req, res) => {
   console.log(req.body)
     const infosPdf = {
         client_name : req.body.clientInfo.client_name,
